e2771f0031164cedc21cfa4a163e887905c72906
add <babak.abad@gmail.com>
2024-06-04 00:19:13 +0330

    refactor

--- time_series/sine_wave_estimatation/main.py
+++ time_series/sine_wave_estimatation/main.py
@@ -3,15 +3,22 @@ import torch
 from torch import nn
 import numpy as np
 from torch.optim import Adam
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+import util as utl
 
 x = np.linspace(0, 1, 50).reshape((-1, 1)).astype('float32')
 y = np.power(x, 2).reshape((-1, 1)).astype('float32')
 
-x = torch.tensor(x)
-y = torch.tensor(y)
+n_batch = 6
+n_epoch = 1000
+
+trn_dl = utl.create_dataloader(x, y, n_batch, True)
+vld_dl = utl.create_dataloader(x, y, n_batch, True)
+tst_dl = utl.create_dataloader(x, y, n_batch, True)
 
 model = nn.Sequential(
-    nn.LSTM(1, 5, fi),
+    nn.Linear(1, 1),
     nn.Sigmoid(),
     nn.Linear(1, 1),
     nn.Sigmoid()
@@ -20,18 +27,16 @@ model = nn.Sequential(
 loss = nn.MSELoss()
 opt = Adam(model.parameters(), lr=0.1)
 model.train()
-n_batch = 6
-n_epoch = 1000
 
-for i in range(n_epoch):
-    for b in range(0, len(x), n_batch):
-        inp = x[b:b+n_batch]
-        out = y[b:b+n_batch]
-        opt.zero_grad()
-        pred = model(inp)
-        ls = loss(pred, out)
-        ls.backward()
-        opt.step()
+utl.train(
+    mdl=model,
+    train_dataloader=trn_dl,
+    valid_dataloader=vld_dl,
+    n_epoch=n_epoch,
+    opt=opt,
+    loss=loss,
+    valid_step=100,
+    save_path='')
 
 model.eval()
 with torch.inference_mode():
@@ -40,8 +45,3 @@ with torch.inference_mode():
 
 plt.plot(x, y)
 plt.show()
-
-
-
-
-
